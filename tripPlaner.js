86d6f69008f73e1a83ad2301551a21c9232c7c5f
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-07 01:11:10 +0600

    practice project

--- tripPlaner.js
+++ tripPlaner.js
@@ -6,7 +6,7 @@ var husband ={
     isMarried: true,
     isBike: true,
     isHouse: true,
-    budgetForTrip: 20000,
+    budgetForTrip: 5000,
     preferredDestination: ['cox-bazar', 'sylhet', 'sajek']
 }
 var wife ={
@@ -16,7 +16,7 @@ var wife ={
     isMarried: true,
     isBike: true,
     isHouse: true,
-    budgetForTrip: 15000,
+    budgetForTrip: 7000,
     preferredDestination: ['sreemangal', 'rangamati', 'sajek']
 }
 var sister ={
@@ -26,7 +26,7 @@ var sister ={
     isMarried: false,
     isBike: false,
     isHouse: true,
-    budgetForTrip: 5000,
+    budgetForTrip: 7000,
     preferredDestination: ['cox-bazar', 'tanguar-haor', 'sajek']
 }
 var shali1 ={
@@ -49,6 +49,42 @@ var shali2 ={
     budgetForTrip: 2000,
     preferredDestination: ['cox-bazar', 'tangail', 'sajek']
 }
+let dayTrip={
+    place: 'Shuborno-Gram',
+    budget: 5000,
+    isWeekEnd:true,
+   foodItem:{
+        breakFast: [],
+        lunch: [],
+        snacks: [],
+        dinner: [],
+    },
+    activities:{
+        isPlayZone:true,
+        isSwimmingPool: true,
+        isOutDoorGames:true
+    }
+}
 console.log("*******************Our Next Trip Planer********************")
 let totalTripBudget = (husband.budgetForTrip+wife.budgetForTrip+sister.budgetForTrip+shali1.budgetForTrip+shali2.budgetForTrip);
-console.log("Total Budget for their next trip is =", totalTripBudget);
+console.log("Total Budget for their next trip is =", totalTripBudget, 'Taka');
+
+// tour condition
+if(totalTripBudget >= 55000){
+    console.log("We will go to Cox's Bazar");
+}
+else if(totalTripBudget >=40000){
+    if(sister.budgetForTrip + shali1.budgetForTrip === 15000){
+        console.log("We will go for where you guys want.");
+    }
+    else{
+        console.log("We will go next time");
+    }
+}
+else{
+    console.log("We will go our long trip next year in sha Allah.");
+    console.log("Let's go for a day long trip near about dhaka city.");
+    console.log(dayTrip);
+
+    }
+
